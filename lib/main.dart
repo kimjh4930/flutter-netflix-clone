1e23d221f13238956256a3f498e73a6ef478ffef
Junha Kim <kimjh4930@gmail.com>
2021-02-02 02:10:13 +0900

    TabController 화면구성

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:netflix_clone/widget/bottom_bar.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TabController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Netflix',
+      theme: ThemeData(brightness: Brightness.dark, accentColor: Colors.white),
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          body: TabBarView(
+            physics: NeverScrollableScrollPhysics(),
+            children: <Widget>[
+              Container(child: Center(child: Text('홈')),),
+              Container(child: Center(child: Text('검색')),),
+              Container(child: Center(child: Text('북마크')),),
+              Container(child: Center(child: Text('더보기')),)
+            ],
+          ),
+          bottomNavigationBar: BottomBar(),
+        ),
+      ),
+    );
+  }
+}
